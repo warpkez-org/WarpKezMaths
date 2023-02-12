3a746b0189e6f82431eec41b4d3b05263c85e6df
warpkez <warp_kez@hotmail.com>
2023-02-13 08:38:43 +1000

    Updates

---
+++ WarpKezMaths/Fractions/FractionMathematics.cs
@@ -0,0 +1,104 @@
+﻿using WarpKezMaths.Models;
+
+namespace WarpKezMaths.Fractions;
+
+
+public class FractionMathematics
+{
+    public static FractionModel ComplexFraction(CompoundFractionModel compoundFraction)
+    {
+        return new FractionModel
+        {
+            numerator = compoundFraction.unit * compoundFraction.denominator + compoundFraction.numerator,
+            denominator = compoundFraction.denominator
+        };
+    }
+
+    public static FractionModel ComplexFraction(int Unit, int Numerator, int Denominator)
+    {
+        return new FractionModel
+        {
+            numerator = Unit * Denominator + Numerator,
+            denominator = Denominator
+        };
+    }
+
+    public static CompoundFractionModel CompoundFraction(FractionModel Fraction)
+    {
+        int _unit = Math.Abs(Fraction.numerator / Fraction.denominator);
+
+        return new()
+        {
+            unit = _unit,
+            numerator = Fraction.numerator - _unit * Fraction.denominator,
+            denominator = Fraction.denominator
+        };
+    }
+
+    public static CompoundFractionModel CompoundFraction(int Numerator, int Denominator)
+    {
+        int _unit = Math.Abs(Numerator / Denominator);
+
+        return new()
+        {
+            unit = _unit,
+            numerator = Numerator - _unit * Denominator,
+            denominator = Denominator
+        };
+    }
+
+    public static CompoundFractionModel SimplifyFraction(int Numerator, int Denominator)
+    {
+        int HighComDivisor = 0;
+        int[] values = new int[2];
+        CompoundFractionModel simplifiedFraction = new();
+
+        // If the numerator is 0 why are we even here then?
+        if (Numerator > 0)
+        {
+            for (int _HighComDivisor = 1; _HighComDivisor <= Denominator; _HighComDivisor++)
+            {
+                // Check for highest common denominator if both have no remainder than _HighComDivisor is a common divisor
+                if (Numerator % _HighComDivisor == 0 && Denominator % _HighComDivisor == 0)
+                {
+                    HighComDivisor = _HighComDivisor;
+                }
+
+                // Test if dividing by zero otherwise return the simplified fraction
+                if (HighComDivisor == 0)
+                {
+                    values[0] = HighComDivisor;
+                    values[1] = HighComDivisor;
+                }
+                else
+                {
+                    // Divide both the numerator and the denominator by the highest common divisor
+                    values[0] = Numerator / HighComDivisor;
+                    values[1] = Denominator / HighComDivisor;
+                }
+            }
+        }
+        else
+        {
+            values[0] = Numerator;
+            values[1] = Denominator;
+        }
+
+        if (values[0] > values[1])
+        {
+            int unit = Math.Abs(values[0] / values[1]);
+            int newNumerator = values[0] - unit * values[1];
+            simplifiedFraction.unit = unit;
+            simplifiedFraction.numerator = newNumerator;
+            simplifiedFraction.denominator = values[1];
+        }
+        else
+        {
+            simplifiedFraction.unit = 0;
+            simplifiedFraction.numerator = values[0];
+            simplifiedFraction.denominator = values[1];
+        }
+
+        return simplifiedFraction;
+    }
+}
---
+++ WarpKezMaths/Trigonometry/TrigonometryMathematics.cs
@@ -0,0 +1,42 @@
+﻿using WarpKezMaths.Models;
+
+namespace WarpKezMaths.Trigonometry;
+
+public class TrigonometryMathematics
+{
+    /// <summary>
+    /// Calculates the Opposite from the Hypotenuse and Adjacent
+    /// </summary>
+    /// <param name="hypotenuse"></param>
+    /// <param name="adjacent"></param>
+    /// <returns>The length of the Opposite line</returns>
+    public static double RightAngleTriangleOpposite(double hypotenuse, double adjacent) => Math.Sqrt((hypotenuse * hypotenuse) - (adjacent * adjacent));
+
+    /// <summary>
+    /// Calculates the adjacent from the Hypotenyse and Opposite
+    /// </summary>
+    /// <param name="hypotenuse"></param>
+    /// <param name="opposite"></param>
+    /// <returns>The length of the Adjacent line</returns>
+    public static double RightAngleTriangleAdjacent(double hypotenuse, double opposite) => Math.Sqrt((hypotenuse * hypotenuse) - (opposite * opposite));
+
+    /// <summary>
+    /// Calculates the Hypotenuse from Adjacent and Opposite
+    /// </summary>
+    /// <param name="adjacent"></param>
+    /// <param name="opposite"></param>
+    /// <returns>The length of the Hypotenuse line</returns>
+    public static double RightAngleTriangleHypotenuse(double adjacent, double opposite) => Math.Sqrt((adjacent * adjacent) + (opposite * opposite));
+
+    public static double SinA(double opposite, double hypotenuse) => opposite / hypotenuse;
+
+    public static double CosA(double adjacent, double hypotenuse) => adjacent / hypotenuse;
+
+    public static double TanA(double opposite, double adjacent) => opposite / adjacent;
+
+    public static double ArcSinA(double opposite, double hypotenuse) => Math.Asin(opposite / hypotenuse);
+
+    public static double ArcCosA(double adjacent, double hypotenuse) => Math.Acos(adjacent / hypotenuse);
+
+    public static double ArcTanA(double opposite, double adjacent) => Math.Atan(opposite / adjacent);
+}
